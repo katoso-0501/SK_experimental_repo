c6f6a1a13eeeb774a97f0cc4f1967fd9f80998bc
So Kato <katoso@dscl.jp>
2025-11-26 14:59:50 +0900

    251126 コーディネータ更新

--- coordinator/assets/js/main.js
+++ coordinator/assets/js/main.js
@@ -7,8 +7,8 @@ class Jonny {
         this.jonnyBody = document.createElement('div');
         this.jonnyBody.classList.add('jonny__body');
 
-        const img = document.createElement('img');
-        img.src = "assets/images/jonny_original.webp";
+        this.img = document.createElement('img');
+        this.img.src = "assets/images/jonny_original.webp";
 
         const tops = document.createElement('div'); 
         tops.classList.add('jonny__tops');
@@ -69,7 +69,7 @@ class Jonny {
             // controllerMaster.append(paragraph);
         });
         
-        this.jonnyBody.append(img,tops,trouser);
+        this.jonnyBody.append(this.img,tops,trouser);
         this.jonnyMain.append(this.jonnyBody);
         this.jonnyMain.append(controllerMaster);
 
@@ -118,60 +118,11 @@ class Jonny {
 class Jimmy extends Jonny {
     constructor() {
         super();
+        this.img.src = "assets/images/jimmy_original.webp";
     }
 }
 
-const characters = [new Jonny(),new Jimmy()];
-
-const tops = document.querySelector('.jonny__tops');
-const trouser = document.querySelector('.jonny__trouser');
-
-const jonny_tops_hsl = [50,50,100];
-const jonny_trouser_hsl = [50,50,14];
-
-document.querySelector('.jonny__tops_hue').addEventListener('change', m=>{
-    const hue = m.target.value;
-    jonny_tops_hsl[0] = hue;
-    changeHSL(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
-});
-document.querySelector('.jonny__tops_saturation').addEventListener('change', m=>{
-    const saturation = m.target.value;
-    jonny_tops_hsl[1] = saturation;
-    changeHSL(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
-});
-document.querySelector('.jonny__tops_brightness').addEventListener('change', m=>{
-    const brightness = m.target.value;
-    jonny_tops_hsl[2] = brightness;
-    changeHSL(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
-});
-
-
-document.querySelector('.jonny__trouser_hue').addEventListener('change', m=>{
-    const hue = m.target.value;
-    jonny_trouser_hsl[0] = hue;
-    changeHSL(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
-});
-document.querySelector('.jonny__trouser_saturation').addEventListener('change', m=>{
-    const saturation = m.target.value;
-    jonny_trouser_hsl[1] = saturation;
-    changeHSL(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
-});
-document.querySelector('.jonny__trouser_brightness').addEventListener('change', m=>{
-    const brightness = m.target.value;
-    jonny_trouser_hsl[2] = brightness;
-    changeHSL(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
-});
-
-
-function changeHSL (tgt,h,s,l) {
-    tgt.style.background = `hsl(${h},${s}%,${l}%)`;
-}
-
-window.addEventListener('load', ()=>{
-    changeHSL(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
-    changeHSL(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
-});
-
+const characters = [new Jonny()];
 
 document.querySelector('.printBtn').addEventListener('click', ()=>{
     window.print();
