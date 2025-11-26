bf38925b2354146ac549b677fadca9cdac489fdf
So Kato <katoso@dscl.jp>
2025-11-26 14:32:37 +0900

    251126 コーディネータ更新

--- coordinator/assets/js/main.js
+++ coordinator/assets/js/main.js
@@ -14,11 +14,98 @@ class Jonny {
         tops.classList.add('jonny__tops');
         const trouser = document.createElement('div');
         trouser.classList.add('jonny__trouser');
+
+        const controllerMaster = document.createElement('div');
+        controllerMaster.classList.add('jonny__controller');
+
+        const controllerDiv = [
+            ["tops",document.createElement('div')],
+            ["trouser",document.createElement('div')],
+        ]
+
+        controllerDiv.forEach(div=>{
+            div[1].classList.add(div[0]);
+            controllerMaster.append(div[1]);
+        })
+
+
+        // Create Controller's Parts
+        const controllerParts =
+        [
+            ['Hue',document.createElement('input')],
+            ['Saturation',document.createElement('input')],
+            ['Brightness',document.createElement('input')],
+            ['Hue',document.createElement('input')],
+            ['Saturation',document.createElement('input')],
+            ['Brightness',document.createElement('input')],
+        ]
+        
+        controllerParts[0][1].classList.add('jonny__tops_hue');
+        controllerParts[1][1].classList.add('jonny__tops_saturation');
+        controllerParts[2][1].classList.add('jonny__tops_brightness');
+        controllerParts[3][1].classList.add('jonny__trouser_hue');
+        controllerParts[4][1].classList.add('jonny__trouser_saturation');
+        controllerParts[5][1].classList.add('jonny__trouser_brightness');
+
+        controllerParts.forEach((part, i) => {
+            const paragraph = document.createElement('p');
+
+            part[1].setAttribute('type', 'range');
+            part[1].setAttribute('min', '0');
+            part[1].setAttribute('max', '100');
+            
+            paragraph.append(part[0],part[1]);
+            if(i < 3){
+                controllerDiv[0][1].append(paragraph);
+            }else{
+                controllerDiv[1][1].append(paragraph);
+            }
+            // controllerMaster.append(paragraph);
+        });
         
         this.jonnyBody.append(img,tops,trouser);
         this.jonnyMain.append(this.jonnyBody);
+        this.jonnyMain.append(controllerMaster);
+
+        
+        controllerParts[0][1].addEventListener('change', m=>{
+            const hue = m.target.value;
+            jonny_tops_hsl[0] = hue;
+            this.HSLupdate(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
+        });
+        controllerParts[1][1].addEventListener('change', m=>{
+            const saturation = m.target.value;
+            jonny_tops_hsl[1] = saturation;
+            this.HSLupdate(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
+        });
+        controllerParts[2][1].addEventListener('change', m=>{
+            const brightness = m.target.value;
+            jonny_tops_hsl[2] = brightness;
+            this.HSLupdate(tops,jonny_tops_hsl[0],jonny_tops_hsl[1],jonny_tops_hsl[2]);
+        });
+
+
+        controllerParts[3][1].addEventListener('change', m=>{
+            const hue = m.target.value;
+            jonny_trouser_hsl[0] = hue;
+            this.HSLupdate(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
+        });
+        controllerParts[4][1].addEventListener('change', m=>{
+            const saturation = m.target.value;
+            jonny_trouser_hsl[1] = saturation;
+            this.HSLupdate(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
+        });
+        controllerParts[5][1].addEventListener('change', m=>{
+            const brightness = m.target.value;
+            jonny_trouser_hsl[2] = brightness;
+            this.HSLupdate(trouser,jonny_trouser_hsl[0],jonny_trouser_hsl[1],jonny_trouser_hsl[2]);
+        });
 
         document.querySelector('body').append(this.jonnyMain);
+    }
+
+    HSLupdate (tgt, h, s, l) {
+        tgt.style.background = `hsl(${h},${s}%,${l}%)`;
     }
 }
 
@@ -28,7 +115,7 @@ class Jimmy extends Jonny {
     }
 }
 
-const characters = [new Jonny()];
+const characters = [new Jonny(),new Jimmy()];
 
 const tops = document.querySelector('.jonny__tops');
 const trouser = document.querySelector('.jonny__trouser');
