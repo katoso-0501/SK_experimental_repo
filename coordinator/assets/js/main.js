f49a610ff2f6bde0323fba6a823e45be09ccbc3a
So Kato <katoso@dscl.jp>
2025-11-27 11:18:50 +0900

    251127 コーディネータ更新

--- coordinator/assets/js/main.js
+++ coordinator/assets/js/main.js
@@ -10,89 +10,15 @@ class Jonny {
         this.img = document.createElement('img');
         this.img.src = "assets/images/jonny_original.webp";
 
-        const tops = document.createElement('div'); 
-        tops.classList.add('char__tops');
-        const trouser = document.createElement('div');
-        trouser.classList.add('char__trouser');
-
-        const controllerMaster = document.createElement('div');
-        controllerMaster.classList.add('char__controller');
-
-        const controllerDiv = [
-            ["Tops",document.createElement('div')],
-            ["Trouser",document.createElement('div')],
-        ]
-
-        controllerDiv.forEach(div=>{
-            div[1].classList.add(div[0]);
-            div[1].innerHTML = "<span>" + div[0] + "</span>";
-            controllerMaster.append(div[1]);
-        })
-
-
-        // Create Controller's Parts
-        const controllerParts =
-        [
-            ['Hue',document.createElement('input')],
-            ['Saturation',document.createElement('input')],
-            ['Brightness',document.createElement('input')],
-            ['Hue',document.createElement('input')],
-            ['Saturation',document.createElement('input')],
-            ['Brightness',document.createElement('input')],
-        ]
+        this.controllerMaster = document.createElement('div');
+        this.controllerMaster.classList.add('char__controller');
         
-        controllerParts[0][1].classList.add('char__tops_hue');
-        controllerParts[1][1].classList.add('char__tops_saturation');
-        controllerParts[2][1].classList.add('char__tops_brightness');
-        controllerParts[3][1].classList.add('char__trouser_hue');
-        controllerParts[4][1].classList.add('char__trouser_saturation');
-        controllerParts[5][1].classList.add('char__trouser_brightness');
-
-        controllerParts.forEach((part, i) => {
-            const paragraph = document.createElement('p');
-
-            part[1].setAttribute('type', 'range');
-            part[1].setAttribute('min', '0');
-            if(i === 0 || i ===  3) {
-                part[1].setAttribute('max', '360');
-            }else{
-
-                part[1].setAttribute('max', '100');
-            }
-            
-            paragraph.append(part[0],part[1]);
-            if(i < 3){
-                controllerDiv[0][1].append(paragraph);
-            }else{
-                controllerDiv[1][1].append(paragraph);
-            }
-        });
-        
-        this.jonnyBody.append(this.img,tops,trouser);
+        this.jonnyBody.append(this.img);
         this.jonnyMain.append(this.jonnyBody);
-        this.jonnyMain.append(controllerMaster);
-
+        this.jonnyMain.append(this.controllerMaster);
         
-        controllerParts[0][1].addEventListener('change', m=>{
-            this.HSLupdate(tops,controllerParts[0][1].value,controllerParts[1][1].value,controllerParts[2][1].value);
-        });
-        controllerParts[1][1].addEventListener('change', m=>{
-            this.HSLupdate(tops,controllerParts[0][1].value,controllerParts[1][1].value,controllerParts[2][1].value);
-        });
-        controllerParts[2][1].addEventListener('change', m=>{
-            this.HSLupdate(tops,controllerParts[0][1].value,controllerParts[1][1].value,controllerParts[2][1].value);
-        });
-
-
-        controllerParts[3][1].addEventListener('change', m=>{
-            this.HSLupdate(trouser,controllerParts[3][1].value,controllerParts[4][1].value,controllerParts[5][1].value);
-        });
-        controllerParts[4][1].addEventListener('change', m=>{
-            this.HSLupdate(trouser,controllerParts[3][1].value,controllerParts[4][1].value,controllerParts[5][1].value);
-        });
-        controllerParts[5][1].addEventListener('change', m=>{
-            this.HSLupdate(trouser,controllerParts[3][1].value,controllerParts[4][1].value,controllerParts[5][1].value);
-        });
+        this.createControls('Tops','char__tops');
+        this.createControls('Trouser','char__trouser');
 
         document.querySelector('.character_wrapper').append(this.jonnyMain);
     }
@@ -101,15 +27,48 @@ class Jonny {
         tgt.style.background = `hsl(${h},${s}%,${l}%)`;
     }
 
-    createControls () {
-        const controllerMaster = document.createElement('div');
-        controllerMaster.classList.add('char__controller');
+    createControls (tgtname,tgt) {
+        const targetPart = document.createElement('div');
+        targetPart.classList.add(tgt);
         
+        const controllerPanel = document.createElement('div');
+        controllerPanel.innerHTML = "<span>" + tgtname + "</span>";
+        controllerPanel.classList.add(tgtname);
+
+
         const controlParts = [
             ['Hue',document.createElement('input')],
             ['Saturation',document.createElement('input')],
             ['Brightness',document.createElement('input')],
         ];
+
+        controlParts[0][1].addEventListener('change', m=>{
+            this.HSLupdate(targetPart,controlParts[0][1].value,controlParts[1][1].value,controlParts[2][1].value);
+        });
+        controlParts[1][1].addEventListener('change', m=>{
+            this.HSLupdate(targetPart,controlParts[0][1].value,controlParts[1][1].value,controlParts[2][1].value);
+        });
+        controlParts[2][1].addEventListener('change', m=>{
+            this.HSLupdate(targetPart,controlParts[0][1].value,controlParts[1][1].value,controlParts[2][1].value);
+        });
+        
+        controlParts.forEach((part, i) => {
+            const paragraph = document.createElement('p');
+
+            part[1].setAttribute('type', 'range');
+            part[1].setAttribute('min', '0');
+            if(i === 0 || i ===  3) {
+                part[1].setAttribute('max', '360');
+            }else{
+                part[1].setAttribute('max', '100');
+            }
+            
+            paragraph.append(part[0],part[1]);
+            controllerPanel.append(paragraph); 
+        });
+        
+        this.controllerMaster.append(controllerPanel);
+        this.jonnyBody.append(targetPart);
     }
 
     deleteCharacter () {
