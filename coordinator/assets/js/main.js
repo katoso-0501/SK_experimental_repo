327e90927edfc93b35c94eb22467248b07e9620d
So Kato <katoso@dscl.jp>
2025-11-27 16:21:30 +0900

    251127 コーディネータ更新

--- coordinator/assets/js/main.js
+++ coordinator/assets/js/main.js
@@ -1,8 +1,11 @@
 "use strict";
+let characterID = 0;
 class Jonny {
     constructor() {
+        characterID++;
         this.jonnyMain = document.createElement('div');
         this.jonnyMain.classList.add('jonny');
+        this.jonnyMain.classList.add('charID_'+characterID);
         
         this.jonnyBody = document.createElement('div');
         this.jonnyBody.classList.add('char__body');
@@ -32,7 +35,7 @@ class Jonny {
         this.createControls('Skin','char__skin');
     }
 
-    createControls (tgtname,tgt) {
+    createControls (tgtname, tgt) {
         const targetPart = document.createElement('div');
         targetPart.classList.add(tgt);
         
@@ -44,7 +47,7 @@ class Jonny {
 
         controllerLabel.addEventListener('click', ()=>{
             controllerPanel.classList.toggle('expanded');
-        })
+        });
 
         const controlParts = [
             ['Hue',document.createElement('input')],
@@ -69,8 +72,10 @@ class Jonny {
             part[1].setAttribute('min', '0');
             if(i === 0 || i ===  3) {
                 part[1].setAttribute('max', '360');
+                part[1].setAttribute('value', Math.floor(Math.random() * 360));
             }else{
                 part[1].setAttribute('max', '100');
+                part[1].setAttribute('value', Math.floor(Math.random() * 100));
             }
             
             paragraph.append(part[0],part[1]);
@@ -79,7 +84,12 @@ class Jonny {
         
         this.controllerMaster.append(controllerPanel);
         this.jonnyBody.append(targetPart);
-        
+        this.HSLupdate(
+            targetPart,
+            controlParts[0][1].value,
+            controlParts[1][1].value,
+            controlParts[2][1].value
+        );
     }
 
     deleteCharacter () {
@@ -91,6 +101,10 @@ class Jimmy extends Jonny {
     constructor() {
         super();
         this.img.src = "assets/images/jimmy_original.webp";
+
+        this.jonnyMain.classList.remove('jonny');
+
+        this.jonnyMain.classList.add('jimmy');
     }
     
     loadIndependentParts () {
@@ -100,7 +114,28 @@ class Jimmy extends Jonny {
     }
 }
 
-const characters = [new Jonny()];
+
+const characters = [];
+
+window.addEventListener('load', ()=>{
+    characters.push(new Jonny());
+    
+    if(window.innerWidth > 768) {
+        document.querySelectorAll('.char__controller > div').forEach(controller=>{
+            controller.classList.add('expanded');
+        })
+    }
+});
+
+document.querySelector('.character_adder__jonny').addEventListener('click', b=>{
+    b.preventDefault();
+    characters.push(new Jonny());
+});
+
+document.querySelector('.character_adder__jimmy').addEventListener('click', b=>{
+    b.preventDefault();
+    characters.push(new Jimmy());
+})
 
 document.querySelector('.printBtn').addEventListener('click', ()=>{
     window.print();
